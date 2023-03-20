94082ea430cd89dd6ac3a846bada32da25426c58
andreatamezm <atamez@dons.usfca.edu>
2023-03-19 23:13:16 -0700

    Initial Commit

---
+++ Rio/RioApp.swift
@@ -0,0 +1,17 @@
+//
+//  RioApp.swift
+//  Rio
+//
+//  Created by Andrea Tamez on 3/19/23.
+//
+
+import SwiftUI
+
+@main
+struct RioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
